9b76e93a19be67568c7b3601912ee2e6d719883a
Edwin <ewn.francisco@gmail.com>
2025-11-10 22:37:09 -0300

    configurando o uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-const imagemin = require('gulp-imagemin')
+const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+
+function scripts() {
+    return gulp.src('./src/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'))
+}
 
 function styles() {
     return gulp.src('./src/styles/*.scss') //apenas para recuperar os arquivos
@@ -14,7 +21,8 @@ function images() {
         .pipe(gulp.dest('./dist/images'));
 }
 
-exports.default = gulp.parallel(styles, images);
+exports.default = gulp.parallel(styles, images, scripts);
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles)) //arquivos observados e a função executada em parelelo, (styles) é a função acima de compilação
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
 }
